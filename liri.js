e42c8cfdf6c1b5231199f7575ae9242af5caaa63
jamie-jessi <jamiejudi@gmail.com>
2017-07-23 22:22:04 -0500

    added comments

--- liri.js
+++ liri.js
@@ -1,15 +1,15 @@
-var fs = require('fs');
-var request = require('request');
+var fs = require('fs'); //file system
+var request = require('request'); //npm packages
 var Spotify = require('node-spotify-api');
 var twitter = require('twitter');
 
-var keys = require('./keys.js');
+var keys = require('./keys.js'); //grabs keys for twitter
 
-var action = process.argv[2];
-var value = process.argv[3];
+var action = process.argv[2]; //grabs user arguement / action
+var value = process.argv[3]; //grabs specific search item
 
 
-switch (action) {
+switch (action) { //if / else for user arguement input. calls proper function
     case "my-tweets":
         twitterFunction();
         break;
@@ -27,37 +27,37 @@ switch (action) {
         break;
 }
 
-function twitterFunction() {
+function twitterFunction() { //twitter function
 
-    var twitterKeys = keys.twitterKeys;
+    var twitterKeys = keys.twitterKeys; //keys needed for file
     var client = new twitter({
         consumer_key: twitterKeys.consumer_key,
         consumer_secret: twitterKeys.consumer_secret,
         access_token_key: twitterKeys.access_token_key,
         access_token_secret: twitterKeys.access_token_secret
     });
-    var handleName = { screen_name: 'jaybiz03', count: 20 };
+    var handleName = { screen_name: 'jaybiz03', count: 20 }; //provides name for api call
     client.get('statuses/user_timeline', handleName, function(error, tweets, response) {
         if (error) {
             return console.log('Error occurred: ' + err);
         }
 
-        for (var i = 0; i < tweets.length; i++) {
-            console.log(tweets[i].text + " created at " + tweets[i].created_at);
-            console.log("(╯°□°）╯︵ ┻━┻)");
+        for (var i = 0; i < tweets.length; i++) { //loops through response data
+            console.log(tweets[i].text + " created at " + tweets[i].created_at); //console logs result
+            console.log("(╯°□°）╯︵ ┻━┻)"); //i do not use twitter
         }
 
     });
 }
 
-function spotifyFunction() {
-    var spotify = new Spotify({
+function spotifyFunction() { //spotify function
+    var spotify = new Spotify({ //keys
         id: 'd65ef172b2f14af9815b15b1332fd0ec',
         secret: '37676f67c2fb4f0da5797d7ce93ebe0e'
     });
 
     var songSearch;
-    if (value === undefined) {
+    if (value === undefined) { //if else statement if there is no argv[3] or specific item
         songSearch = 'The Sign ace of base';
     } else {
         songSearch = value;
@@ -68,7 +68,7 @@ function spotifyFunction() {
             return console.log('Error occurred: ' + err);
         }
 
-        for (i = 0; i < data.tracks.items.length; i++) {
+        for (i = 0; i < data.tracks.items.length; i++) { //loops through results and console logs the data
             console.log('');
             console.log('Artist Name: ' + data.tracks.items[i].artists[0].name);
             console.log('Track Name: ' + data.tracks.items[i].name);
@@ -78,11 +78,11 @@ function spotifyFunction() {
     });
 };
 
-function omdbFunction() {
+function omdbFunction() { //movie function
 
     var movie;
 
-    if (value === undefined) {
+    if (value === undefined) { //if else statement if there is no argv[3] or specific item
         movie = 'Mr. Nobody';
     } else {
         movie = value;
@@ -92,7 +92,7 @@ function omdbFunction() {
     request(movieUrl, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             var results = JSON.parse(body)
-            console.log('ლ(ಠ益ಠლ)');
+            console.log('ლ(ಠ益ಠლ)'); //used this api for project. did not play well with results
             console.log(results);
             console.log('Title: ' + results.Title);
             console.log('');
@@ -114,8 +114,8 @@ function omdbFunction() {
     });
 }
 
-function randomFunction() {
-    fs.readFile('random.txt', 'utf8', function(err, data) {
+function randomFunction() { //random function
+    fs.readFile('random.txt', 'utf8', function(err, data) { //grabs data from random.text file
 
         if (err) {
             return console.log('Error occurred: ' + err);
